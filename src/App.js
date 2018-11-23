5028a24e6d7d7f7bc2082a4ba877481216184950
wcolts2000 <wcolts2000@gmail.com>
2018-11-22 20:30:34 -0800

    added reusable toggle component and portal component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Toggle from "./components/Toggle";
+import Portal from "./components/Portal";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Toggle>
+          {({ on, toggle }) => (
+            <>
+              {on && <h1>SHOW ME</h1>}
+              <button onClick={toggle}>SHOW / HIDE</button>
+              <Portal>{on && <h1>I'm in a portal</h1>}</Portal>
+            </>
+          )}
+        </Toggle>
       </div>
     );
   }
